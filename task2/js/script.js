0fc87d7d1773169260724a1202f149f42d5e2850
Shukhrat <snuper25@gmail.com>
2021-02-09 11:02:55 +0600

    task1

---
+++ task2/js/script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+
+let money = +prompt('Ваш бюджет на месяц?', ''),
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+
+};
+
+let   a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+      b = +prompt('Во сколько обойдется?', ''),
+      c = prompt('Введите обязательную статью расходов в этом месяце', ''),
+      d = +prompt('Во сколько обойдется?', '');
+
+appData.expenses[a] = b;
+appData.expenses[c] = d;
+
+console.log(appData.budget / 30);
+alert(appData.budget / 30);
